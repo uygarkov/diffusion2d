ad53b0510bafdd921165f07feb90c6408b6ad348
Uygar <83709209+uygarkov@users.noreply.github.com>
2022-11-02 14:05:58 +0100

    First commit

---
+++ uygarkov/output.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure()
+
+
+def output_plots(im):
+
+    # Plot output figures
+    fig.subplots_adjust(right=0.85)
+    cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
+    cbar_ax.set_xlabel('$T$ / K', labelpad=20)
+    fig.colorbar(im, cax=cbar_ax)
+    plt.show()
+
+
+def create_plot(u0, u, D, dt, dx2, dy2, T_hot, T_cold, fig_counter):
+    # Time loop
+    ax = fig.add_subplot(220 + fig_counter)
+    im = ax.imshow(u.copy(), cmap=plt.get_cmap('hot'), vmin=T_cold, vmax=T_hot)  # image for color bar axes
+    return ax, im
+
+
+
+
+
+
+
+
+
